0484cb37059c75d87ca2bfa65fcc85bc5afe8da6
Abhishek Yadav <abhishekyadav@Abhisheks-MacBook-Pro.local>
2024-01-30 16:53:11 +0530

    added price and variant in people also bought card

--- src/components/PeopleAlsoBoughtCard.jsx
+++ src/components/PeopleAlsoBoughtCard.jsx
@@ -1,4 +1,4 @@
-import { extractProductId } from '@/lib/utils';
+import { extractProductId, formatPrice, truncate } from '@/lib/utils';
 import RatingService from '@/services/rating';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -31,11 +31,11 @@ const PeopleAlsoBoughtCard = ({
   const imageUrl = product?.images?.edges?.[0]?.node?.url;
   return (
     <div
-      className={`flex flex-col gap-1 items-start justify-between pb-2 min-w-[220px] w-[220px] h-[350px] p-3 rounded-xl bg-white`}
+      className={`flex flex-col gap-1 items-start justify-between pb-2 min-w-[220px] w-[220px] h-[380px] p-3 rounded-xl bg-white`}
     >
       <Link
         href={`/products/${product.handle}`}
-        className="flex flex-col items-start gap-1"
+        className="flex flex-col items-start gap-1 flex-1"
         onClick={() => {
           setIsSheetOpen(false);
           setSheetKey(sheetKey + 1);
@@ -68,6 +68,12 @@ const PeopleAlsoBoughtCard = ({
           size="15"
           className={'text-primary text-sm'}
         />
+        <div className="font-questrial text-left text-sm font-bold text-secondary flex items-center gap-2 mt-auto">
+          {formatPrice(product?.variants?.edges?.[0]?.node?.price?.amount)}
+          <span className="text-gray-600 text-xs font-medium">
+            ({truncate(product?.variants?.edges?.[0]?.node?.title, 20)})
+          </span>
+        </div>
       </Link>
       <div className="py-2 w-full">
         <AddToCartButton
