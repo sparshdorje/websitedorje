e94fe5aa8137ce9c00483b3471b7890ccf397a71
Abhishek Yadav <abhishekyadav@Abhisheks-MacBook-Pro.local>
2024-02-23 20:14:13 +0530

    created experience page

---
+++ src/config/RestaurantsOfSelimHill.js
@@ -0,0 +1,30 @@
+import { ASSETS } from './index';
+
+export const RESTAURANTS_OF_SELIM_HILL = [
+  {
+    name: "Glenary's",
+    image: `${ASSETS.RESTAURANTS}/glenary.webp`,
+    blogUrl: '/blogs/posts/no-one-can-take-my-freedom-away',
+  },
+  {
+    name: "Keventer's",
+    image: `${ASSETS.RESTAURANTS}/keventers.webp`,
+    blogUrl:
+      '/blogs/posts/the-hoopoe-selim-hill-tea-estate-s-israel-connection',
+  },
+  {
+    name: 'Kunga Restaurant',
+    image: `${ASSETS.RESTAURANTS}/kunga.webp`,
+    blogUrl: '/blogs/posts/sunshine-on-the-wing',
+  },
+  {
+    name: 'The Park',
+    image: `${ASSETS.RESTAURANTS}/park.webp`,
+    blogUrl: '/blogs/posts/one-flew-over-the-kalij-s-nest',
+  },
+  {
+    name: 'Dekevas Restaurant',
+    image: `${ASSETS.RESTAURANTS}/dekevas.webp`,
+    blogUrl: '/blogs/posts/who-gives-a-hoot',
+  },
+];
